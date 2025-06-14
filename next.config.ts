5134666948028fb169a943678b45578e706dce4d
Wiku Karno Prasetyagama <prasetyagama2@gmail.com>
2025-06-15 01:54:49 +0700

    Adds dynamic support for remote image patterns
    Introduces dynamic remotePatterns configuration for Next.js images, allowing domains to be sourced from environment variables.
    
    Enhances flexibility by incorporating custom asset URLs in addition to local development patterns.

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,36 @@
 import type { NextConfig } from "next";
+import type { RemotePattern } from "next/dist/shared/lib/image-config";
+
+// Ambil domain dari environment variable
+const imageDomain = process.env.NEXT_PUBLIC_ASSET_URL;
+
+const remotePatterns: RemotePattern[] = [
+  {
+    protocol: "http",
+    hostname: "127.0.0.1",
+    port: "8000",
+    pathname: "/storage/**",
+  },
+  {
+    protocol: "http",
+    hostname: "localhost",
+    port: "8000",
+    pathname: "/storage/**",
+  },
+];
+
+// Tambahkan domain dari ENV kalau ada
+if (imageDomain) {
+  remotePatterns.push({
+    protocol: "https",
+    hostname: imageDomain,
+    pathname: "/storage/**",
+  });
+}
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "8000",
-        pathname: "/storage/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
-        pathname: "/storage/**",
-      },
-    ],
+    remotePatterns,
   },
 };
 
